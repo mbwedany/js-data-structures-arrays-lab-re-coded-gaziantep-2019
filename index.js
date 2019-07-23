bfb453526d7cf4b29e295bebcb28380f38583879
Mohamad Bwidani <mbwedany@gmail.com>
2019-07-23 11:06:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendDriver(){
+function destructivelyAppendDriver(palph){
   drivers.push('Ralph')
 }
 
